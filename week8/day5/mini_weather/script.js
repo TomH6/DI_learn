1dbf8ff588599c154af53f78860ddd160409bdb2
TomH6 <tomthehein@gmail.com>
2022-02-22 14:23:38 +0200

    22.222

--- week8/day5/mini_weather/script.js
+++ week8/day5/mini_weather/script.js
@@ -1,12 +1,45 @@
-let form = document.querySelector('form');
+const form = document.querySelector('form');
+const clear = document.querySelector('#clear');
+const bottom = document.getElementById('bottom');
+let celOrFar = document.querySelector(`input[type=checkbox]`);
 
-let kToC =(currentTempKal)=>{
-    let curTempC =  currentTempKal -  273.15;
-    return Math.round(curTempC);
+const celForFar = (e)=>{
+    [...document.querySelectorAll('.temp')].forEach(p=>{
+        kToC(parseFloat(p.textContent),p)})
+   
+}
+
+
+const kToC =(currentTempKal,p)=>{
+    console.log(currentTempKal);
+    let celOrFarVal = celOrFar.checked; 
+    const celcius = currentTempKal -273.15;
+    const farenheit = celcius * (9/5) +32;
+    console.log(farenheit);
+    let curTempC;
+    let unit = celOrFarVal
+    ? 'F'
+    : 'C'
+    if(!p){
+        curTempC = celOrFarVal
+        ?farenheit.toFixed(2)
+        :celcius.toFixed(0)
+    }
+    else{
+        curTempC = !celOrFarVal
+        ?celcius.toFixed(0)
+        :farenheit.toFixed(2)
+    }
+    
+    let toReturn = `${curTempC} °${unit}`;
+    if(p){p.textContent = toReturn};
+    return toReturn;
+    
 };
 
-let wrongPlace=(cityName)=>{
+const wrongPlace=(cityName)=>{
     let wrongDiv = document.createElement('div');
+    wrongDiv.id = 'divError';
     let wrongHeader = document.createElement('h3');
     wrongHeader.textContent = ` ${cityName} is NOT A real place`;
     wrongDiv.appendChild(wrongHeader);
@@ -14,10 +47,16 @@ let wrongPlace=(cityName)=>{
     form.reset();
 }
 
-let createCityCard = (info)=>{   
-    let bottom = document.getElementById('bottom');
+let createCityCard = (info)=>{
+    let wrongDiv = document.querySelector('#divError')
+    if(wrongDiv){
+        wrongDiv.remove();}
+    
     let card = document.createElement("div");
     card.classList.add('card');
+    let del = document.createElement('button');
+    del.classList.add('btn-close');
+    del.addEventListener('click',()=>card.remove());
     let weatherIcon = info.list[0].weather[0].icon;
     let icon = document.createElement('img');
     icon.classList.add('card-img-top');     
@@ -30,8 +69,9 @@ let createCityCard = (info)=>{
     let currentTempKal = info.list[0].main.temp;
     let curTempC = kToC(currentTempKal);
     let temp = document.createElement('p');
-    temp.classList.add('card-text');
-    temp.textContent = `${curTempC}°C`;
+
+    temp.classList.add('card-text', 'temp');
+    temp.textContent = `${curTempC}`;
     let currentHum = info.list[0].main.humidity;
     let humid = document.createElement('p');
     humid.classList.add('card-text')  ;
@@ -40,6 +80,7 @@ let createCityCard = (info)=>{
     let desc = document.createElement('p');
     desc.classList.add('card-text');
     desc.textContent = description;
+    card.appendChild(del);
     card.appendChild(icon);
     cardBody.appendChild(cardTitle);
     cardBody.appendChild(temp);
@@ -50,26 +91,14 @@ let createCityCard = (info)=>{
 };
 
 
-// let button = document.querySelector("#submit");
-//     let CountButton = 0;
-//     button.addEventListener("click", function() {
-//       CountButton += 1;
-//       console.log(CountButton);
-//     });
-// let replaceDiv = (CountButton)=>{
-//     if (CountButton >= 1){
-//         card.classList.add('d-none');
-//     }  
-// };
-
-
-let retriveCity =(e)=>{
+const retriveCity =(e)=>{
     e.preventDefault();
     let form = e.target;
     let cityName = form.citySearch.value;
     let url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&id=524901&appid=6bc236fa8bd5e7e03f83fd8cea3eac74`;
     let xhr = new XMLHttpRequest();
     xhr.open('GET',url,true);
+    if(cityName){
     xhr.onload = function(){
         if(xhr.status == 200){
             let xhrTobj = JSON.parse(xhr.response) ;     
@@ -78,12 +107,13 @@ let retriveCity =(e)=>{
         } else {
             wrongPlace(cityName)
         }  
-    }
+    }}
     xhr.send();
     
 };
 form.addEventListener('submit', retriveCity);
-
+clear.addEventListener('click', ()=>bottom.innerHTML = "");
+celOrFar.addEventListener('change', celForFar);
 
 
 
