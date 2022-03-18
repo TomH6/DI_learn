e37cb36fec7f536ee26f46a5c04dbfda9e3a3a1f
TomH6 <tomthehein@gmail.com>
2022-03-18 14:35:51 +0200

    18.3.22.b

---
+++ week12/day4/daily_c/public/app.js
@@ -0,0 +1,16 @@
+
+// fetch('http://localhost:3000/aboutMe/:hobby')
+// .then(res=>res.json())
+// .then(data =>{
+//     console.log(data);
+//     (document.querySelector('div')).textContent = JSON.stringify(data);
+// })
+// .catch(e=>console.log(e));
+
+// fetch('http://localhost:3000/aboutMe/:hobby')
+// .then(res=>res.json())
+// .then(data =>{
+//     console.log(data);
+//     (document.querySelector('div')).textContent = JSON.stringify(data);
+// })
+// .catch(e=>console.log(e));
---
+++ week12/day4/daily_c/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/aboutMe/:hobby', (req, res) => {
+  if (typeof req.params.hobby != 'string'){
+    res.status(404).send("not found")
+    console.log('404 Page not found')
+  } else {
+    res.json(req.params.hobby)
+    console.log(req.params.hobby)
+  }
+});
+
+app.get('/pic', (req, res) => {
+    res.sendFile('/public/pic.html', {root: __dirname})
+    console.log('sending pic page')
+});
+
+app.get('/formData', (req, res) => {
+    let email = req.query.email;
+    let message = req.query.message;
+    res.end(`${email} sent you a message: ${message}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`) 
+});
