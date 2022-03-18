5402ab7b9a2d3ecd043f10677c4780a27dda77bf
TomH6 <tomthehein@gmail.com>
2022-03-18 12:54:34 +0200

    18.3.22

---
+++ week12/day4/EX_two/public/script.js
@@ -0,0 +1,19 @@
+//** PART 1 **//
+
+// fetch('http://localhost:3000/user')
+// .then(res=>res.json())
+// .then(data =>{
+//     console.log(data);
+//     (document.querySelector('div')).textContent = JSON.stringify(data);
+// })
+// .catch(e=>console.log(e));
+
+//** PART 2 **//
+
+// fetch('http://localhost:3000/:id')
+// .then(res=>res.json())
+// .then(data =>{
+//     console.log(data);
+//     (document.querySelector('div')).textContent = JSON.stringify(data);
+// })
+// .catch(e=>console.log(e));
---
+++ week12/day4/EX_two/server.js
@@ -0,0 +1,49 @@
+//** PART 1 **//
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// const user = {
+//     firstname: 'John',
+//     lastname: 'Doe'
+// };
+// app.use('/', express.static(__dirname + '/public'));
+
+// app.get('/user', (req, res) => {
+//   res.json(user)
+//   console.log(user);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`) 
+// });
+
+//** PART 2 **//
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// app.use('/', express.static(__dirname + '/public'));
+
+// app.get('/:id', (req, res) => {
+//     res.json(req.params)
+//     console.log(req.params);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`) 
+// });
+
+//** PART 3 **//
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.use('/', express.static(__dirname + '/public'));
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`) 
+// });
+
+
